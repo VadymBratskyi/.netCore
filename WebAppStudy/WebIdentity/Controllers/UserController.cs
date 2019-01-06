564609a3e1f85d1aac7e1835b2e07be8c7c4e534
Vadim Bratskyi <vadymbratskyi@gmail.com>
2019-01-07 01:12:08 +0200

    user

---
+++ WebAppStudy/WebIdentity/Controllers/UserController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebIdentity.Models;
+using WebIdentity.ViewModels;
+
+namespace WebIdentity.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_userManager.Users.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User(){UserName = model.Email, Email = model.Email, Year = model.Year};
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var identityError in result.Errors)
+                    {
+                        ModelState.AddModelError("",identityError.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            EditUserViewModel model = new EditUserViewModel(){Id = user.Id, Email = user.Email, Year = user.Year};
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.Email = model.Email;
+                    user.UserName = model.Email;
+                    user.Year = model.Year;
+
+                    var result = await _userManager.UpdateAsync(user);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var identityError in result.Errors)
+                        {
+                            ModelState.AddModelError("", identityError.Description);
+                        }
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                await _userManager.DeleteAsync(user);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> ChangePassword(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            ChangePasswordViewModel model = new ChangePasswordViewModel(){Email = user.Email, Id = user.Id};
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Пользователь не найден");
+                }
+                else
+                {
+                    //var _passworValidator = 
+                    //    HttpContext.RequestServices.GetService(typeof(IPasswordValidator<User>)) as IPasswordValidator<User>;
+
+                    //var _passwordHasher =
+                    //    HttpContext.RequestServices.GetService(typeof(IPasswordHasher<User>)) as IPasswordHasher<User>;
+
+                    //var result = await _passworValidator.ValidateAsync(_userManager, user, model.NewPassword);
+
+                    IdentityResult result =
+                        await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+
+                    if (result.Succeeded)
+                    {
+                        //user.PasswordHash = _passwordHasher.HashPassword(user, model.NewPassword);
+                        //await _userManager.UpdateAsync(user);
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var identityError in result.Errors)
+                        {
+                            ModelState.AddModelError("", identityError.Description);
+                        }
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
