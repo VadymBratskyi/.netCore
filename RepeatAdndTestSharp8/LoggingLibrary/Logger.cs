9915ff52f1ea16212b5f137a480cf55a9d2b2d2e
Vadim Bratskyi <vadymbratskyi@gmail.com>
2019-11-06 14:58:21 +0200

    moks

---
+++ RepeatAdndTestSharp8/LoggingLibrary/Logger.cs
@@ -0,0 +1,27 @@
+﻿using LoggingLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingLibrary
+{
+    public class Logger
+    {
+        private ISensitiveDataScruber _sensitiveDataScruber;
+        private IMessageBodyGenerator _messageBodyGenerator;
+        private IMessageHeaderGenerator _messageHeaderGenerator;
+        private ILoggingConfiguration _loggingConfiguration;
+
+        public Logger (ISensitiveDataScruber sensitiveDataScruber, IMessageBodyGenerator messageBodyGenerator,
+            IMessageHeaderGenerator messageHeaderGenerator, ILoggingConfiguration loggingConfiguration)
+        {
+            _sensitiveDataScruber = sensitiveDataScruber;
+            _messageBodyGenerator = messageBodyGenerator;
+            _messageHeaderGenerator = messageHeaderGenerator;
+            _loggingConfiguration = loggingConfiguration;
+        }
+        
+    }
+}
---
+++ RepeatAdndTestSharp8/LoggingLibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LoggingLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LoggingLibraryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c12cf650-a3aa-4eb3-a4b0-cb65b36d82d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
